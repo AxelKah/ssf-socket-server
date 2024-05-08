5ece30cbd741f2bd23b109c77fecf51a2c998615
AxelKah <axel.kahkonen@gmail.com>
2024-05-08 13:48:37 +0300

    for testing

--- src/functions/gameToDB.ts
+++ src/functions/gameToDB.ts
@@ -4,12 +4,14 @@ import { doGraphQLFetch } from "../graphql/fetch";
 
 require("dotenv").config(); // Loading environment variables from .env file
 
-
-const apiUrl = process.env.API_URL as string; // API URL from environment variables
+//hardcoded for testing
+//const apiUrl = process.env.API_URL as string; // API URL from environment variables
+const apiUrl = "https://axelkah-darts.azurewebsites.net/graphql";
 
 const sendGametoDB = async (data: Array<any>, currentTurn: string) => {
 console.log("data to server   täälläää  näin satasarfas: ", data);
-  const token = process.env.TOKEN as string;
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjNiNTM1NGE2YmIwMDRkNjA3NzhkYjMiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJ1c2VyX25hbWUiOiJ0ZXN0Iiwicm9sZSI6InVzZXIiLCJpYXQiOjE3MTUxNjQ0OTV9.eqeVZ-ToSVKKdq7he2PTEa3rFaRCVUABnAzU6RU-hjY";  
+//const token = process.env.TOKEN as string;
   try {
     if (data.length > 0) {
       // Check if the data array is not empty
